7b9591219fbf7ec5b40e554938f7351c356ea3b2
CodeAadarsh <aadarsh24lv@gmail.com>
2023-10-02 04:17:35 +0530

    almost done with frontend

--- src/app/MyComponents/recipe/recipe-list/recipe-list.component.ts
+++ src/app/MyComponents/recipe/recipe-list/recipe-list.component.ts
@@ -1,9 +1,16 @@
 import { Component } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
     selector:'app-recipe-list',
     templateUrl:'recipe-list.component.html'
 })
 export class RecipeListComponent{
-    
+    recipes: Recipe[] = [
+        new Recipe('A Recipe Name','Recipe Decription','https://www.cookingcarnival.com/wp-content/uploads/2017/07/Pav-bhaji-768x1024.webp'),   
+        new Recipe('A Recipe Name','Recipe Decription','https://www.cookingcarnival.com/wp-content/uploads/2017/07/Pav-bhaji-768x1024.webp')
+]
+    constructor(){
+        console.log(this.recipes)
+    }
 }
--- src/app/MyComponents/shopping/shopping.component.ts
+++ src/app/MyComponents/shopping/shopping.component.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import { Ingredients } from "../Shared/ingredients.model";
 
 @Component({
     selector:'app-shopping',
     templateUrl:'shopping.component.html'
 })
 export class ShoppingComponent {
+    ingredients:Ingredients[] = [
+        new Ingredients('Apples',10),
+        new Ingredients('Tamatoes',10)
+]
 
 }
