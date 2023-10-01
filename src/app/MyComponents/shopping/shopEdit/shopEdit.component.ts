c74dc27acd263d954efb79e2cc8289ef4040ec17
CodeAadarsh <aadarsh24lv@gmail.com>
2023-10-01 19:45:14 +0530

    initial project setup

---
+++ src/app/MyComponents/shopping/shopEdit/shopEdit.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-shopEdit',
+    templateUrl:'shopEdit.component.html'
+})
+export class shopEditComponent {
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './MyComponents/header/header.component';
+import { ShoppingComponent } from './MyComponents/shopping/shopping.component';
+import { RecipeComponent } from './MyComponents/recipe/recipe.component';
+import { RecipeListComponent } from './MyComponents/recipe/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './MyComponents/recipe/recipe-detail/recipe-detail.component';
+import { shopEditComponent } from './MyComponents/shopping/shopEdit/shopEdit.component';
+import { RecipeItemComponent } from './MyComponents/recipe/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingComponent,
+    RecipeComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    shopEditComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
